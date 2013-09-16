3a0682a3189cb59679d21d86eb946d8253b6a8d4
jz <jgzhang@hotmail.com>
2013-09-16 16:07:27 -0700

    initial commit

---
+++ authagent.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+
+"""
+Running on local host, listening on https over tcp and http over unix domain socket
+for authentication assertions and secure temporary storage of authorization assertions
+"""
+import os
+import struct
+import socket
+import http.server
+import socketserver
+import ssl
+import threading
+import tempfile
+import sock2proc
+import logging
+
+from socketserver import TCPServer, UnixStreamServer, ForkingMixIn, ForkingTCPServer
+from http.server import SimpleHTTPRequestHandler
+from sock2proc import ProcInfo
+
+LOCAL_PORT = 6443 
+LOCAL_PATH = tempfile.gettempdir() + "/cloudauth.sk"
+
+logger = logging.getLogger("authagent")
+
+class CloudAuthHTTPReqHandler(SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+
+        proc = ProcInfo.sock2proc(self.client_address)
+
+        SimpleHTTPRequestHandler.do_GET(self); 
+
+    def do_POST(self):
+
+        proc = ProcInfo.sock2proc(self.client_address)
+
+        SimpleHTTPRequestHandler.do_POST(self); 
+ 
+class HttpsThread (threading.Thread):
+
+    def __init__(self, port, isTCP):
+
+        threading.Thread.__init__(self)
+        self.port = port
+        self.isTCP = isTCP 
+
+    def run(self):
+
+        handler = CloudAuthHTTPReqHandler
+
+        httpd = None
+
+        if (self.isTCP):         
+
+            class MyForkingTCPServer(ForkingTCPServer):
+                 def server_bind(self):
+                     self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                     self.socket.bind(self.server_address)
+
+            httpd = MyForkingTCPServer(("", self.port), handler) #TODO "" -> localhost
+
+            httpd.socket = ssl.wrap_socket (httpd.socket, keyfile='./ssh_host_rsa_key', certfile='./ssh_host_rsa_key.crt', server_side=True)
+ 
+        else :
+
+            if (os.path.exists(self.port)):
+                os.unlink(self.port)
+            class ForkingUsServer(ForkingMixIn, UnixStreamServer): pass
+            httpd = ForkingUsServer(self.port, handler)
+
+        logger.info("serving at port %s", self.port) 
+
+
+        httpd.serve_forever()
+
+# Entrance for stand-alone execution
+def main():
+    
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s %(message)s', level=logging.INFO)
+
+    logger.info("cloudauth is running.")
+    
+    threads = []
+
+    threads.append(HttpsThread(LOCAL_PORT, True));
+    threads.append(HttpsThread(LOCAL_PATH, False));
+
+    #TODO create unix domain thread
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.start() 
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join() #TODO restart if returned
+
+    logger.info ("cloudauth is shutdown") 
+ 
+if __name__ == "__main__":
+    
+    main()
+
---
+++ sock2proc.py
@@ -0,0 +1,207 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import pwd
+import struct
+import socket
+import hashlib
+import logging
+
+logger = logging.getLogger("sock2proc")
+
+class ProcInfo(object):
+    
+    sock = -1 # used to find proc info vi proc/etc/net
+    
+    pid = -1
+    euid = -1
+    user = ""
+    cmd = ""
+    arg = ""
+    env = ""
+    clnt = ""
+    binh = ""
+    bindir = ""   
+
+    # Either sock (local tcp) or pid/euid (unix stream) must be present
+    def __init__(self, sock = -1, pid = -1, euid = -1):
+        self.sock = sock
+        self.pid = pid
+        self.euid = euid
+        
+            
+    def proc_info(self):
+
+        # Either pid or sock must be available
+
+        if (self.pid == -1):
+            if (self.sock ==""):
+                return False       
+            else:
+                inode, self.euid = ProcInfo.find_inode(self.sock)
+                if (inode == -1):
+                    return False
+
+                self.pid = ProcInfo.find_pid(inode)
+                if (self.pid == -1):
+                    return False
+               
+        self.user = pwd.getpwuid(int(self.euid))[0];
+        logger.info("user = %s", self.user)
+
+        fname = open("/proc/" + self.pid + "/cmdline", "r")
+        self.cmd = fname.readline()
+        self.cmd = self.cmd.strip()
+        fname.close()
+
+        if (len(self.cmd) != 0):
+             argv = self.cmd.split("\0")
+             logger.info("proc %s argv: %s", self.pid, argv)
+             self.cmd = argv[0] 
+             for i in range(1, len(argv)):
+                 self.arg += argv[i] + " "
+             self.arg = self.arg.strip(' ')
+       
+        #Special case where args part of command
+        #TODO bug if space in filename (escaping)
+        idx = self.cmd.find(" ")
+        if (idx > 0):
+            self.arg += self.cmd[(idx + 1):].strip()
+            self.cmd = self.cmd[:idx].strip()
+ 
+        logger.info("clnt exe: %s", self.cmd)
+        logger.info("clnt arg: %s", self.arg)
+
+        self.bindir, self.cmd = os.path.split(self.cmd)
+              
+        logger.info("clnt bin dir: %s", self.bindir)
+
+        self.clnt = self.cmd + "@" + socket.getfqdn() + "#" + self.user
+        logger.info("clnt id: %s", self.clnt) 
+
+        self.binh = ProcInfo.hash_file("/proc/" + self.pid + "/exe")
+ 
+        logger.info("clnt exe hash: %s",self.binh) 
+
+        fname = open("/proc/" + self.pid + "/environ", "r")
+        self.env = fname.readline()
+        fname.close()
+        self.env = self.env.replace("\0", "\n").strip()
+        logger.info("clnt env: %s", self.env)
+
+        return True
+
+    # Find the inode of a local sock address    
+    @staticmethod
+    def find_inode(sock):
+        
+        inode = -1
+        euid = -1;
+        
+        fname = open("/proc/net/tcp", "r")
+        fname.readline() # discard the 1st line
+        while (True):
+            line = fname.readline();
+            if not line:
+                break
+            items = line.split()
+            if (items[1].find(sock.upper()) != -1) :
+                euid = items[7]
+                inode = items[9]
+                break
+            
+        fname.close()
+        
+        logger.info("euid = %s", euid)
+        
+        if (euid == -1):
+            return -1, -1 
+    
+
+        return int(inode), euid
+    
+    # Find the pid given its inode
+    @staticmethod
+    def find_pid(inode):
+            
+        files = os.listdir("/proc/")
+    
+        files.remove(str(os.getpid()))
+    
+        pids = []
+    
+        for f in files:
+            try:
+                integer = int(f)
+                pids.append(str(integer))
+            except ValueError:
+                # don't care if not a pid
+                pass
+    
+        for pid in pids:
+            fds = os.listdir("/proc/%s/fd/" % pid)
+            for fd in fds:
+                if ('socket:[%d]' % inode) == os.readlink("/proc/%s/fd/%s" % (pid, fd)):
+                    return pid
+        return -1
+ 
+    @staticmethod 
+    def hash_file(file):
+
+        hash = ""
+
+        with open(file, 'rb') as fh:
+            m = hashlib.sha1()
+            while True:
+                data = fh.read(8192)
+                if not data:
+                    break
+                m.update(data)
+            hash = m.hexdigest()
+
+        return hash 
+ 
+    @staticmethod
+    def ip_to_nl(ip):
+        t = socket.inet_aton(ip)
+        return struct.unpack("I", t)[0]
+
+    @staticmethod
+    def sock2proc(client_address):
+
+        logger.info ("client sock: %s", client_address);
+
+        host, port = client_address
+
+        clnt = hex(ProcInfo.ip_to_nl(host)).lstrip('0x') + ":" + hex(int(port)).lstrip('0x')
+
+        proc = ProcInfo(sock = clnt)
+
+        proc.proc_info()
+
+        return proc
+
+    @staticmethod
+    def pipe2proc(pid, euid):
+
+        logger.info ("client pid: %s, euid: %s", pid, euid);
+
+        proc = ProcInfo(pid = pid, euid = euid)
+
+        proc.proc_info()
+
+        return proc
+
+ 
+# Entrance for stand-alone execution
+def main():
+
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s %(message)s', level=logging.INFO) 
+
+    proc = ProcInfo.pipe2proc(sys.argv[1], sys.argv[2])  
+
+if __name__ == "__main__":
+    
+    main()
+    
